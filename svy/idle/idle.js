01b634e57b39ce8f00131ec91ce15a2ac409cb85
tuanway <tuanway@gmail.com>
2018-05-14 16:54:28 -0500

    update doc

--- svy/idle/idle.js
+++ svy/idle/idle.js
@@ -10,8 +10,8 @@ angular.module('svyIdle',['servoy'])
 		 * @param {function} onHide triggers when window is hidden
 		 * @param {function} onShow	triggers when window is shown
 		 * @param {string} events string of events that will reset idle time (default : 'mousemove keydown mousedown touchstart')
-		 * @param {Number} idle idle time in ms
-		 * @param {Boolean} keepTracking set to false if we onl ywant to track the first time (true by default)
+		 * @param {Number} idle idle time in ms, default: 60000
+		 * @param {Boolean} keepTracking set to false if we only want to track the first time (true by default)
 		 * @param {Boolean} startAtIdle if you want to start at idle, set to true
 		 * @param {Boolean} recurIdleCall use setInterval versus timeout, by default uses setTimeout
 		 */
